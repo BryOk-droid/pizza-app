2a9a30498ce53b6a303c13364af25c6fdec3fccf
BryOk-droid <brianokoth534@gmail.com>
2025-06-14 20:03:15 +0300

    add root route to display welcome message at '/'"

--- server/app.py
+++ server/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from server.models import db
-from server.controllers.pizza_controller import pizza_bp  
+from server.controllers.pizza_controller import pizza_bp
+from server.controllers.restaurant_controller import restaurant_bp
+from server.controllers.restaurant_pizza_controller import restaurant_pizza_bp
 
 def create_app():
     app = Flask(__name__)
@@ -9,6 +11,14 @@ def create_app():
 
     db.init_app(app)
 
-    app.register_blueprint(pizza_bp) 
+   
+    @app.route('/')
+    def home():
+        return {'message': 'Welcome to the Pizza API'}
+
+    
+    app.register_blueprint(pizza_bp)
+    app.register_blueprint(restaurant_bp)
+    app.register_blueprint(restaurant_pizza_bp)
 
     return app
